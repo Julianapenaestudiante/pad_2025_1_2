7adbe926d5300cf2b7fcf188786e2bfc7ed590c2
julianaIUD <juliana.pena@iudigital.edu.co>
2025-05-08 22:57:08 -0500

    creación del scraping

---
+++ src/edu_pad/main.py
@@ -0,0 +1,19 @@
+
+from dataweb import DataWeb
+
+import pandas as pd
+
+
+
+def main():
+    dataweb = DataWeb()
+
+    df = dataweb.obtener_datos()
+    df = dataweb.convertir_numericos(df)
+ 
+ 
+
+
+
+if __name__ == "__main__":
+    main()
