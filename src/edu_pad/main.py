978b7a20b85c75ca37c254a3b0afbb59b84f8b34
julianaIUD <juliana.pena@iudigital.edu.co>
2025-05-25 18:46:51 -0500

    cambios main

--- src/edu_pad/main.py
+++ src/edu_pad/main.py
@@ -5,27 +5,27 @@ def main():
     dataweb = DataWeb()
     df = dataweb.obtener_datos()
 
-    #  Mostrar los datos extraídos
-    print("\n Datos extraídos de Yahoo Finance:")
+    print("\n--- Datos extraidos de Yahoo Finance ---")
     if df.empty:
-        print(" No se obtuvieron datos.")
+        print("No se obtuvieron datos.")
         return
     else:
         print(df.head(10))  # Mostrar solo las primeras 10 filas
 
-        #Estadísticas básicas
-        print(f"\n Total de filas extraídas: {len(df)}")
-        print(f" Rango de fechas: {df['fecha'].min()} → {df['fecha'].max()}")
+        # Estadísticas básicas
+        print(f"\nTotal de filas extraidas: {len(df)}")
+        print(f"Rango de fechas: {df['fecha'].min()} hasta {df['fecha'].max()}")
 
-        #Guardar el archivo CSV
+        # Guardar el archivo CSV
         output_dir = "static/csv"
         os.makedirs(output_dir, exist_ok=True)
         csv_path = os.path.join(output_dir, "data_web.csv")
         df.to_csv(csv_path, index=False)
 
-        print(f"\n✅ CSV generado correctamente en: {csv_path}")
+        print(f"\nCSV generado correctamente en: {csv_path}")
 
 if __name__ == "__main__":
     main()
+
 
 
